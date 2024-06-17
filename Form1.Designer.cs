85631da7ff185a080a949d90012f21f3e18a12fe
duykhang02092002 <khang54212310002@bachkhoasaigon.edu.vn>
2024-06-17 16:00:11 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace csharp_tinhlaixuat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btntinh = new System.Windows.Forms.Button();
+            this.txtsotien = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtlaisuat = new System.Windows.Forms.TextBox();
+            this.btnketqua = new System.Windows.Forms.ListBox();
+            this.txtsonangui = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "số tiền ";
+            // 
+            // btntinh
+            // 
+            this.btntinh.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntinh.Location = new System.Drawing.Point(55, 372);
+            this.btntinh.Name = "btntinh";
+            this.btntinh.Size = new System.Drawing.Size(120, 48);
+            this.btntinh.TabIndex = 1;
+            this.btntinh.Text = "tính";
+            this.btntinh.UseVisualStyleBackColor = true;
+            this.btntinh.Click += new System.EventHandler(this.btntinh_Click);
+            // 
+            // txtsotien
+            // 
+            this.txtsotien.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsotien.Location = new System.Drawing.Point(217, 79);
+            this.txtsotien.Name = "txtsotien";
+            this.txtsotien.Size = new System.Drawing.Size(145, 33);
+            this.txtsotien.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "lãi suất";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(70, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "số năm gửi ";
+            // 
+            // txtlaisuat
+            // 
+            this.txtlaisuat.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlaisuat.Location = new System.Drawing.Point(217, 127);
+            this.txtlaisuat.Name = "txtlaisuat";
+            this.txtlaisuat.Size = new System.Drawing.Size(145, 33);
+            this.txtlaisuat.TabIndex = 2;
+            // 
+            // btnketqua
+            // 
+            this.btnketqua.FormattingEnabled = true;
+            this.btnketqua.Location = new System.Drawing.Point(75, 244);
+            this.btnketqua.Name = "btnketqua";
+            this.btnketqua.Size = new System.Drawing.Size(252, 95);
+            this.btnketqua.TabIndex = 3;
+            this.btnketqua.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // txtsonangui
+            // 
+            this.txtsonangui.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsonangui.Location = new System.Drawing.Point(217, 173);
+            this.txtsonangui.Name = "txtsonangui";
+            this.txtsonangui.Size = new System.Drawing.Size(145, 33);
+            this.txtsonangui.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnketqua);
+            this.Controls.Add(this.txtsonangui);
+            this.Controls.Add(this.txtlaisuat);
+            this.Controls.Add(this.txtsotien);
+            this.Controls.Add(this.btntinh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btntinh;
+        private System.Windows.Forms.TextBox txtsotien;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtlaisuat;
+        private System.Windows.Forms.ListBox btnketqua;
+        private System.Windows.Forms.TextBox txtsonangui;
+    }
+}
+
